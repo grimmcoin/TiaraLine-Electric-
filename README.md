6269bd41c330de48fcdb34a50bae66e20ef81094
grimmcoin <grimmcoin321@gmail.com>
2026-06-30 00:09:41 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# TiaraLine-Electric
+
+[![Open in Bolt](https://bolt.new/static/open-in-bolt.svg)](https://bolt.new/~/sb1-c4pu17gh)
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        navy: {
+          50: '#f0f4ff',
+          100: '#e0e9ff',
+          200: '#c7d6fe',
+          300: '#a5b9fc',
+          400: '#8194f8',
+          500: '#636ff1',
+          600: '#4f4ce5',
+          700: '#423dca',
+          800: '#3634a3',
+          900: '#1e2a4a',
+          950: '#0f172a',
+        },
+        gold: {
+          50: '#fffbeb',
+          100: '#fef3c7',
+          200: '#fde68a',
+          300: '#fcd34d',
+          400: '#fbbf24',
+          500: '#f59e0b',
+          600: '#d97706',
+          700: '#b45309',
+          800: '#92400e',
+          900: '#78350f',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+        display: ['Outfit', 'Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
